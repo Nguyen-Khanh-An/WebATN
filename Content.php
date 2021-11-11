0562ca367d03d3cd1775622c8b3188b2234615d4
Nguyen-Khanh-An <91657471+Nguyen-Khanh-An@users.noreply.github.com>
2021-11-11 12:23:33 +0700

    tvc
    xc

--- Content.php
+++ Content.php
@@ -31,13 +31,13 @@
             </div>
           </div>
           <div class="carousel-item">
-            <img src="./tree/img/anh11.png" class="d-block w-100" alt="slide2" width="1000" height="450">
+            <img src="./tree/img/anh11.png" class="d-block w-100" alt="anh11" width="1000" height="450">
             <div class="carousel-caption d-none d-md-block">
               <p>SALE 20%</p>
             </div>
           </div>
           <div class="carousel-item">
-            <img src="./tree/img/anh5.png" class="d-block w-100" alt="slide3" width="1000" height="450">
+            <img src="./tree/img/anh5.png" class="d-block w-100" alt="anh5" width="1000" height="450">
             <div class="carousel-caption d-none d-md-block">
               <p>SALE 50%</p>
             </div>
