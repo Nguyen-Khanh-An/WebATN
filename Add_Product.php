0e147591c076c0cd3726ee346d6743a405ee3674
Nguyen-Khanh-An <91657471+Nguyen-Khanh-An@users.noreply.github.com>
2021-11-11 10:49:34 +0700

    rg
    vd

--- Add_Product.php
+++ Add_Product.php
@@ -56,9 +56,7 @@
 		if (trim($category) == "0") {
 			$err .= "<li>Choose category, Please</li>";
 		}
-		if (!is_numeric($price) || ($price <= 0)) {
-			$err .= "<li>enter price greater than 0 or is number, Please</li>";
-		}
+		
 
 
 		if (!is_numeric($qty) || ($qty <= 0)) {
