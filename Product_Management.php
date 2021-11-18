95baf8a07404047da60a3cdd1c11a973cd066be9
Nguyen-Khanh-An <annkgcc19160@fpt.edu.vn>
2021-11-18 22:47:02 +0700

    et
    ae

--- Product_Management.php
+++ Product_Management.php
@@ -69,9 +69,8 @@
         include_once("conection.php");
         $No = 1;
         $result = pg_query($conn, "SELECT product_id, product_name, price, pro_qty, pro_image, cat_name FROM product a, category b
-                WHERE a.cat_id = b.cat_id ORDER BY prodate DESC");
-        $result = pg_query($conn, "SELECT shop_name FROM public.shops");
-        while ($row = pg_fetch_assoc($result)) {
+                WHERE a.cat_id = b.cat_id ORDER BY prodate DESC"); and $result1 = pg_query($conn, "SELECT shop_name FROM public.shops");
+        while ($row = pg_fetch_assoc($result and $result1)) {
         ?>
           <tr>
             <td><?php echo $No; ?></td>
