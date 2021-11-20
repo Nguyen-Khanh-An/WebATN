fa2aa3901d780b05197b220c87aa353c6e6fbac7
Nguyen-Khanh-An <annkgcc19160@fpt.edu.vn>
2021-11-20 20:54:48 +0700

    test102
    12

--- Product_Management.php
+++ Product_Management.php
@@ -68,7 +68,7 @@
         <?php
         include_once("conection.php");
         $No = 1;
-        $result = pg_query($conn, "SELECT product_id, product_name, price, pro_qty, pro_image, cat_name, shop_name FROM product a, category b, 
+        $result = pg_query($conn, "SELECT product_id, product_name, price, pro_qty, pro_image, cat_name, shop_name FROM public.shops, product a, category b, 
                 WHERE  a.cat_id = b.cat_id ORDER BY prodate DESC");
         while ($row = pg_fetch_assoc($result)) {
         ?>
